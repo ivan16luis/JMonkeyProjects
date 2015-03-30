a45101ba6f5f3913df786d8d4bca0e383a7601fb
ivan16luis <ivanlugiz16@yahoo.com>
2015-03-30 09:00:30 -0500

    Game

--- BasicGameJME/src/mygame/Main.java
+++ BasicGameJME/src/mygame/Main.java
@@ -23,7 +23,9 @@ public class Main extends SimpleApplication {
     public void simpleInitApp() {
         Box b = new Box(1, 1, 1);
         Geometry geom = new Geometry("Box", b);
-        //SALUDOS ESTA ES MI APLICAION 
+        /**
+         * SALUDOS ESTA ES MI APLICAION
+         */
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         mat.setColor("Color", ColorRGBA.Blue);
         geom.setMaterial(mat);
